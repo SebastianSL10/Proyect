08091791b6fba420f537c350eab61ad3f87d854e
Ericksanti32 <119987541+Ericksanti32@users.noreply.github.com>
2023-06-28 12:29:32 -0500

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Formularios from './formularios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <Formularios/>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
