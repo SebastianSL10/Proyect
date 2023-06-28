bf1658c7cbb786a5fb682452232227603749ccd6
Cristian cardona <120037895+CristianC77@users.noreply.github.com>
2023-06-28 12:33:13 -0500

    CRUD tabla_categorias

---
+++ Crud-react/build/precache-manifest.c3bf2a75123c2cf739702bc2d9a9aa43.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059c0105917443227d570cf6a531c32f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd2eef1dab8a1fc41b93",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "dc997c3b4de991c6bba0",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dd2eef1dab8a1fc41b93",
+    "url": "/static/js/2.493321b0.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.493321b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc997c3b4de991c6bba0",
+    "url": "/static/js/main.fa5271ca.chunk.js"
+  },
+  {
+    "revision": "0eca4f07d3188be3b79d",
+    "url": "/static/js/runtime-main.2a38a0dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
